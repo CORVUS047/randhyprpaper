a79e4b215436285f291f11971a4d5af618514f45
Travis McNair <blameless_in_my_time@proton.me>
2024-11-28 14:00:19 -0800

    corrected naming errors

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo "Enter a directory for your wallpapers (absolute path):"
+echo "Enter a directory for your wallpapers:"
 read wallpaperDirectory
 echo "Enter the time in minutes that you want between wallpaper changes:"
 read wallpaperDelay
 echo "env = WALLPAPER_DIR,$wallpaperDirectory" >>~/.config/hypr/hyprland.conf
 echo "env = WALLPAPER_DELAY,$wallpaperDelay" >>~/.config/hypr/hyprland.conf
-echo "exec-once = sleep 9 && randpaper" >>~/.config/hypr/hyprland.conf
+echo "exec-once = sleep 9 && randhyprpaper" >>~/.config/hypr/hyprland.conf
 cmake -GNinja -S . -B build
 cd build
 ninja
-sudo mv randpaper /usr/local/bin
+sudo mv randhyprpaper /usr/local/bin
 echo "
-NOTE: Randpaper requires a restart of hyprland or a reboot.
+NOTE: randhyprpaper requires a restart of hyprland or a reboot.
 If you want to change your wallpaper directory or wallpaper delay
 edit the WALLPAPER_DIR and WALLPAPER_DELAY environment variables 
 in your hyprland.conf file."
